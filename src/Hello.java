3a9c8832fc9cf0bd663cc20f42ca990bfcde0962
Ronald Rajski <ronaldrajski@gmail.com>
2021-05-18 18:39:31 -0500

    starting primitive data types

--- src/Hello.java
+++ src/Hello.java
@@ -3,6 +3,20 @@ public class Hello {
     public static void main(String[] args) {
 //        System.out.println("Hello World");
         System.out.println("Hello Ron");
+
+        int myFirstNumber = 5;
+        int mySecondNumber = (10+5) + (2*10);
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = 1000 - myTotal;
+        System.out.println("myFirstNumber");
+//        String Littoral
+        System.out.println("myFirstNumber: " + myFirstNumber);
+        System.out.println("mySecondNumber: " + mySecondNumber);
+        System.out.println("myFirstNumber + mySecondNumber: " + myFirstNumber + mySecondNumber);
+        System.out.println("myThirdNumber: " + myThirdNumber);
+        System.out.println("myTotal: " + myTotal);
+        System.out.println("myLastOne:" + myLastOne);
     }
 
 
