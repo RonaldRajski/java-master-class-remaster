c87d6d6e9fd1e7fc05fbbabb30988999977183f5
Ronald Rajski <ronaldrajski@gmail.com>
2021-05-18 18:05:38 -0500

    hello world and ron

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    //Main Method
+    public static void main(String[] args) {
+//        System.out.println("Hello World");
+        System.out.println("Hello Ron");
+    }
+
+
+}
+
+
